98f95c5fe3bc5ce6cfe37c68cc83eac3d2c47396
José Leocadio <50723703+JuninhoLeo@users.noreply.github.com>
2021-04-01 09:36:35 -0300

    Update GenericRepository.java

--- DesafioSupply/src/main/java/com/totvs/supplyagro/DesafioSupply/commons/base/domain/GenericRepository.java
+++ DesafioSupply/src/main/java/com/totvs/supplyagro/DesafioSupply/commons/base/domain/GenericRepository.java
@@ -4,12 +4,14 @@ import com.totvs.tjf.api.jpa.repository.ApiJpaRepository;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.ZonedDateTime;
 import java.util.UUID;
 
-@NoRepositoryBean
+@Repository
+@Transactional(readOnly = true)
 public interface GenericRepository<ENTITY extends EntityBase> extends JpaRepository<ENTITY, UUID>, ApiJpaRepository<ENTITY> {
     Page<ENTITY> findByUpdatedAtGreaterThanEqual(Pageable pageable, ZonedDateTime updatedAt);
 }
